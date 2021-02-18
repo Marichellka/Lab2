e429aceb4410defd8e18f0468b05e4510b3c6d66
Marichka <marichka.v.richtsi@gmail.com>
2021-02-18 22:41:18 +0200

    Create method for reading

---
+++ Project/Project/Reader.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Project
+{
+    public class Reader
+    {
+        public static List<string[]> CsvReader(string path)
+        {
+            List<string[]> info =new List<string[]>();
+            string [] files = Directory.GetFiles(path);
+
+            foreach (var currentfFile in files)
+            {
+                using (var sr=new StreamReader(currentfFile, Encoding.Unicode))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        info.Add(sr.ReadLine().Split(','));
+                    }
+                }
+            }
+            return info;
+        }
+    }
+}
