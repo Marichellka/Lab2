acb7905b12f4eb7baa0384950cbb1133f70e229b
Max <rendon4ik@gmail.com>
2021-02-20 19:20:39 +0200

    Output in .csv file format

---
+++ Project/Project/Writer.cs
@@ -0,0 +1,27 @@
+﻿namespace Writer
+{
+    using System;
+    using System.IO;
+    using System.Text;
+
+    public class Writer
+    {
+        public static void CsvWriter(string[,] top)
+        {
+            var str = new StringBuilder();
+            string determinator = ",";
+            string nameOfTheFile = @"C:\result.csv";
+            using (FileStream fs = File.Create(nameOfTheFile))
+            {
+                for (int i = 0; i < top.GetLength(0); i++)
+                {
+                    var name = top[i, 0].ToString();
+                    var mark = top[i, 1].ToString();
+                    var line = name + determinator + mark;
+                    str.AppendLine(line);
+                }
+            }
+            File.WriteAllText(nameOfTheFile, str.ToString());
+        }
+    }
+}
