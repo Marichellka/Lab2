c3b1439b657235260867a1b948d01778d935d8fc
Marichka <marichka.v.richtsi@gmail.com>
2021-02-20 23:21:45 +0200

    Create method SumOfVotes()

---
+++ Project/Project/Processing.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace Project
+{
+    public class Processing
+    {
+        public static Dictionary<string, int> SumOfVotes(List<string[]> info)
+        {
+            Dictionary<string, int> countries=new Dictionary<string, int>();
+
+            foreach (var country in info)
+            {
+                string name = country[0];
+                int sum = 0;
+                
+                for (int i = 1; i < country.Length; i++) 
+                    sum += int.Parse(country[i]);
+                
+                countries.Add(name, sum);
+            }
+            return countries;
+        }
+    }
+}
