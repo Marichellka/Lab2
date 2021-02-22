a3628001fa4a6ecda310eb7371f5722b1b1b52e6
Marichka <marichka.v.richtsi@gmail.com>
2021-02-22 18:12:15 +0200

    Write Main

--- Project/Project/Program.cs
+++ Project/Project/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace Project
 {
@@ -6,7 +8,18 @@ namespace Project
     {
         static void Main(string[] args)
         {
-            
+            Console.Write("Enter the path to te directory: ");
+            string path = Console.ReadLine();
+            while (!Directory.Exists(path))
+            {
+                Console.Write("Directory doesn`t exist.\nPlease, enter the correct path to te directory: ");
+                path = Console.ReadLine();
+            }
+
+            List<string[]> information = Reader.CsvReader(path);
+            string[,] countries = Processing.SumOfVotes(information);
+            countries = Processing.SortTheTop(countries);
+            Writer.CsvWriter(countries);
         }
     }
 }
