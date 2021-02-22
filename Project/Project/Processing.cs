306a70f2b673dad1a45da3f9343ae557592504ad
Max <rendon4ik@gmail.com>
2021-02-22 13:56:17 +0200

    Adding sort method

--- Project/Project/Processing.cs
+++ Project/Project/Processing.cs
@@ -21,5 +21,34 @@ namespace Project
 
             return countries;
         }
+        public static string[,] SortTheTop(string[,] countries)
+        {
+            var item = 1;
+            for (int i = 0; i < countries.GetLength(0); i++)
+            {
+                for (int j = 0; j < countries.GetLength(0) - 1; j++)
+                {
+                    if (Int32.Parse(countries[j, item]) < Int32.Parse(countries[j + 1, item]))
+                    {
+                        var temp = String.Empty;
+                        for (int k = 0; k < countries.GetLength(1); k++)
+                        {
+                            temp = countries[j, k];
+                            countries[j, k] = countries[j + 1, k];
+                            countries[j + 1, k] = temp;
+                        }
+                    }
+                }
+            }
+            var top = new string[10, 2];
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    top[i, j] = countries[i, j];
+                }
+            }
+            return top;
+        }
     }
 }
