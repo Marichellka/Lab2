d513d80524011a7941f4d79d238f981212152878
Marichka <marichka.v.richtsi@gmail.com>
2021-02-23 21:34:36 +0200

    Fix Bugs in Writer.cs

--- Project/Project/Writer.cs
+++ Project/Project/Writer.cs
@@ -1,22 +1,23 @@
 ﻿namespace Project
 {
     using System;
+    using System.Collections.Generic;
     using System.IO;
     using System.Text;
 
     public class Writer
     {
-        public static void CsvWriter(string[,] top, string path)
+        public static void CsvWriter(List<string[]> top, string path)
         {
             var str = new StringBuilder();
             string determinator = ",";
             string nameOfTheFile = path + "\\result.csv";
             using (FileStream fs = File.Create(nameOfTheFile))
             {
-                for (int i = 0; i < top.GetLength(0); i++)
+                for (int i = 0; i < 10; i++)
                 {
-                    var name = top[i, 0].ToString();
-                    var mark = top[i, 1].ToString();
+                    var name = top[i][0];
+                    var mark = top[i][1];
                     var line = name + determinator + mark;
                     str.AppendLine(line);
                 }
